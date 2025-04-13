77fe4f78d98e43fcaad971f67f0cf05c5eda3fa5
Morris73198 <150124400+Morris73198@users.noreply.github.com>
2025-04-13 15:47:17 -0400

    Add files via upload

--- scripts/two_robot_a2c/train.py
+++ scripts/two_robot_a2c/train.py
@@ -5,7 +5,7 @@ from two_robot_a2c.models.multi_robot_trainer import MultiRobotACTrainer
 from two_robot_a2c.environment.multi_robot_no_unknown import Robot
 from two_robot_a2c.config import MODEL_CONFIG, TRAIN_CONFIG, MODEL_DIR
 
-from two_robot_a2c_enhance.environment.robot_local_map_tracker import RobotIndividualMapTracker
+from two_robot_a2c.environment.robot_local_map_tracker import RobotIndividualMapTracker
 
 
 import matplotlib
