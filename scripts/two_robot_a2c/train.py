01abe23cc4ddf1affaca93721bcc2cab817d2f1d
Morris73198 <150124400+Morris73198@users.noreply.github.com>
2025-05-07 23:42:58 +0800

    Add files via upload

--- scripts/two_robot_a2c/train.py
+++ scripts/two_robot_a2c/train.py
@@ -87,7 +87,7 @@ def main():
             # 設置 epsilon 相關參數
             trainer.epsilon = 1.0           # 設置當前的 epsilon 值 (探索率)
             trainer.epsilon_min = 0.075     # 設置最小 epsilon 值
-            trainer.epsilon_decay = 0.9985  # 設置 epsilon 衰減率
+            trainer.epsilon_decay = 0.9989985  # 設置 epsilon 衰減率
             
             # 確保模型保存目錄存在
             if not os.path.exists(MODEL_DIR):
