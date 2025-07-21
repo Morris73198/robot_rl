eb178a6597221bad904751ff0344f601517ad087
Morris73198 <150124400+Morris73198@users.noreply.github.com>
2025-07-21 19:48:14 +0800

    Add files via upload

--- multi_robot_test/__init__.py
+++ multi_robot_test/__init__.py
@@ -1,3 +1,3 @@
 from .environment.multi_robot import Robot
-from .models.multi_robot_trainer import MultiRobotTrainer
-from .models.multi_robot_network import MultiRobotNetworkModel
+from .models.multi_robot_trainer import MultiRobotDQNTrainer
+from .models.multi_robot_network import MultiRobotDQNNetwork
